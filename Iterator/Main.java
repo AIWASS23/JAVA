3eeaa870fa7f258c59230031c1e607fd60619232
AIWASS23 <marcelodearaujo677@gmail.com>
2022-10-29 08:30:07 -0300

    Estudo completo de Iterator

--- Iterator/Main.java
+++ Iterator/Main.java
@@ -1,18 +1,11 @@
-/*
+/**********************************************************************************************
+  * *Comentários: "Iterator"
   *********************************************************************************************
-  * ( )[ ][ ]                                                                                 *
-  * [ ][ ]                                                                                    *
-  * [ ][ ][ ]                                                                                 *
-  * [ ][ ]    - IFCE - Padrões de Projetos - 2021.1 - Prof.Ronaldo 🎷                         *
-  *                                                                                           *
-  *********************************************************************************************
-  * *Comentários: Código implementando o PP do tipo "Iterator"
-  *********************************************************************************************
-  * *Obs. Esse PP apresenta uma maneira de 'acessar os elementos' de um objeto sem expor a toda a estrutura, achei interessante abordar nessa estratégia a imaginando como um 'controle remoto'
+  * *Obs. Esse Projeto apresenta uma maneira de 'acessar os elementos' de um objeto sem expor a toda a estrutura, achei interessante abordar nessa estratégia a imaginando como um 'controle remoto'
   de uma TV, e as classe como canais específicos onde podemos 'acessar' sem necessariamente 
   querer ver 'tudo' que o canal dispõe ao longo do dia por exemplo.
   *********************************************************************************************
-  * * (Definindo o PP - "Iterator") - Segundo a definição da Wikipédia: 
+  * * ( "Iterator" ) - Segundo a definição da Wikipédia: 
   * Na programação orientada a objetos, um iterador se refere tanto ao objeto que permite ao programador percorrer um container, (uma coleção de elementos) particularmente listas,quanto ao design pattern Iterator, no qual um iterador é usado para percorrer um container e acessar seus elementos. O padrão Iterator desacopla os algoritmos dos recipientes, porém em alguns casos, os algoritmos são necessariamente específicos dos containers e, portanto, não podem ser desacoplados.
   *********************************************************************************************
 	*/
@@ -23,7 +16,7 @@ class Main {
 
     System.out.println(" ");
     System.out.println("---------------------------------------------------------------");
-    System.out.println("IFCE - Prof.Ronaldo Ramos 🎷 - PP - Iterator ");
+    System.out.println(" Iterator ");
     System.out.println("---------------------------------------------------------------");
     System.out.println(" ");
 
