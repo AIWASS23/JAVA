1a239892cbfd084e5455f1a1d8bd00d63081f2d0
AIWASS23 <marcelodearaujo677@gmail.com>
2022-11-03 08:15:25 -0300

    Atualização State

--- State/Main.java
+++ State/Main.java
@@ -1,21 +1,14 @@
 /*
+  * *Comentários: "State"
   *********************************************************************************************
-  * ( )[ ][ ]                                                                                 *
-  * [ ][ ]                                                                                    *
-  * [ ][ ][ ]                                                                                 *
-  * [ ][ ]    - IFCE - Padrões de Projetos - 2021.1 - Prof.Ronaldo 🎷                         *
-  *                                                                                           *
-  *********************************************************************************************
-  * *Comentários: Código implementando o PP do tipo "state"
-  *********************************************************************************************
-  * *Obs. Esse PP de forma 'simples' permite que você 'mude o comportamento' de uma classe quando 
+  * *Obs. Esse Projeto de forma 'simples' permite que você 'mude o comportamento' de uma classe quando 
     o determinado estado muda. Na minha opnião é um dos padrões que acho mais divertido pois seu uso
     não só é quase 'infinito' kkkk como há muitas possibilidades divertidas para se usá-lo. Aqui trouxe
     esse exemplo 'óbvio' que pensei logo de cara quando vi o nome do PP, ninguém melhor do que um dos
     personagens mais famosos dos games, nosso amigo Mario!, pra mostrar a essência desse padrão, que
     pelas classes já diz tudo né? kkkkk.
   *********************************************************************************************
-  * * (Definindo o PP - "state") - Segundo a definição da Wikipédia: 
+  * * ("state") - Segundo a definição da Wikipédia: 
   * O padrão State é um padrão comportamental de design de software que implementa uma máquina de estados de maneira orientada à objetos. Com o padrão State, uma máquina de estados é implementada através da implementação de cada um dos estados individuais como uma classe derivada da interface do padrão State, e implementando transições de estado através de chamadas de métodos definidos na superclasse do padrão. O padrão State pode ser interpretado como um padrão de estratégia que é capaz de alterar a estratégia corrente através de invocações de métodos definidos na interface do padrão.
   *********************************************************************************************
 	*/
@@ -35,7 +28,7 @@ class Main {
 
     System.out.println(" ");
     System.out.println("---------------------------------------------------------------");
-    System.out.println("IFCE - Prof.Ronaldo Ramos 🎷 - PP - State ");
+    System.out.println(" State ");
     System.out.println("---------------------------------------------------------------");
     System.out.println(" ");
 
